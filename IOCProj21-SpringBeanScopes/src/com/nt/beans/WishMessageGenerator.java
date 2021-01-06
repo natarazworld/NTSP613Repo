2a5ce7987a1dcd0034a7d9656c26a85c8e1877d2
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-01-06 19:38:39 +0530

    Bean scopes

---
+++ IOCProj21-SpringBeanScopes/src/com/nt/beans/WishMessageGenerator.java
@@ -0,0 +1,14 @@
+//WishMessageGenerator .java
+package com.nt.beans;
+
+public class WishMessageGenerator {
+	
+	public WishMessageGenerator() {
+		System.out.println("WishMessageGenerator:: 0-param constructor");
+	}
+	
+	public  String sayHello(String user) {
+		return "Good Morning:"+user;
+	}
+
+}
---
+++ IOCProj21-SpringBeanScopes/src/com/nt/test/SpringBeanScopsTest.java
@@ -0,0 +1,23 @@
+package com.nt.test;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+import com.nt.beans.WishMessageGenerator;
+
+public class SpringBeanScopsTest {
+
+	public static void main(String[] args) {
+		//create IOC container
+		DefaultListableBeanFactory factory=new DefaultListableBeanFactory();
+		XmlBeanDefinitionReader  reader=new XmlBeanDefinitionReader(factory);
+		reader.loadBeanDefinitions("com/nt/cfgs/applicationContext.xml");
+		//get Spring bean class obj
+		WishMessageGenerator generator=factory.getBean("wmg",WishMessageGenerator.class);
+		WishMessageGenerator generator1=factory.getBean("wmg",WishMessageGenerator.class);
+		WishMessageGenerator generator2=factory.getBean("wmg",WishMessageGenerator.class);
+		System.out.println(generator.hashCode()+" "+generator1.hashCode()+"  "+generator2.hashCode());
+		System.out.println("generator==generator1?"+(generator==generator1));
+		System.out.println("generator1==generator2?"+(generator1==generator2));
+	}
+}
