50af9167b7b9b7f6c2cdb0c319d2410794f0e2b7
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-03-19 20:37:19 +0530

    yml

---
+++ IOCProj66-Boot-ConfigurationProperites-YML/src/main/java/com/nt/model/Person.java
@@ -0,0 +1,29 @@
+package com.nt.model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+@Data
+@Component("pb")
+@ConfigurationProperties(prefix ="info.per" )
+public class Person {
+    private int id;
+	private String name;
+	
+	private int marks[];
+	private List<String> nickNames;
+	 private Set<Long> phoneNumbers;
+	 
+	 private Map<String,Long> idCards;
+	 private Properties favFruits;
+	 
+	
+
+}
