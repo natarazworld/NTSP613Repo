e1b72385bd4fb6f488d00bd5f02bd11d5994ee8f
Nareshit <Nareshit@DESKTOP-IUDAAVL>
2021-04-24 20:29:27 +0530

    Aop Around advice

---
+++ AOPProj01-AroundAdvice-Decl/src/main/java/com/nt/test/AspectjAOPAroundAdviceTest.java
@@ -0,0 +1,26 @@
+package com.nt.test;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.nt.service.BankService;
+
+public class AspectjAOPAroundAdviceTest {
+
+	public static void main(String[] args) {
+		//create IOC container
+		ClassPathXmlApplicationContext ctx=new ClassPathXmlApplicationContext("com/nt/cfgs/applicationContext.xml");
+		//get Proxy class obj
+		BankService proxy=ctx.getBean("bankService",BankService.class);
+		System.out.println(proxy.getClass()+"   "+proxy.getClass().getSuperclass());
+		//invoke the methods
+		System.out.println("Simple Intrest amount::"+proxy.calcSimpleIntrestAmount(40000, 2, 12));
+		System.out.println("---------------------------");
+		System.out.println("compound Intrest amount::"+proxy.calcCompoundIntrestAmount(100000, 2, 12));
+		
+		//close container
+		 ctx.close();
+		
+
+	}
+
+}
